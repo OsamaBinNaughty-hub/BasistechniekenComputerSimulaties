672208e4bff5f6b69b4ad809712b10d70b7f4366
SamClercky <sam.clercky@gmail.com>
2021-02-20 20:59:34 +0100

    newton implemented

--- hoofdstuk2/newton/src/main.rs
+++ hoofdstuk2/newton/src/main.rs
@@ -1,10 +1,12 @@
+use std::time::SystemTime;
+
 use num::pow::pow;
 
-fn newton(func: impl Fn(f32) -> f32, dfunc: impl Fn(f32) -> f32, x0: f32, epsilon: f32, max_iter: u32) -> Result<f32, String> {
+fn newton<T: num::Float>(func: impl Fn(T) -> T, dfunc: impl Fn(T) -> T, x0: T, epsilon: T, max_iter: u32) -> Result<T, String> {
     let mut xn = x0;
-    let mut n = max_iter;
+    let mut max_iter = max_iter;
 
-    while n != 0  {
+    while max_iter != 0  {
         let fxn = func(xn);
 
         if fxn.abs() < epsilon {
@@ -14,19 +16,23 @@ fn newton(func: impl Fn(f32) -> f32, dfunc: impl Fn(f32) -> f32, x0: f32, epsilo
         // bereken voor volgende iteratie
         let df = dfunc(xn);
 
-        if df == 0.0 { return Err(String::from("Afgeleide wordt 0")); }
+        if df == num::zero() { return Err(String::from("Afgeleide wordt 0")); }
         xn = xn - fxn / df;
 
-        n -= 1;
+        max_iter -= 1;
     }
 
     Err(String::from("Te veel iteraties"))
 }
 
 fn main() {
-    let p = |x: f32| {pow(x, 3) - pow(x, 2) - 1.0};
-    let dp = |x: f32| {3.0*pow(x,2) - 2.0*x};
+    let start_time = SystemTime::now();
+
+    let p = |x: f64| {pow(x, 3) - pow(x, 2) - 1.0};
+    let dp = |x: f64| {3.0*pow(x,2) - 2.0*x};
 
     let approx = newton(p, dp, 1.0, 1e-10, 10).unwrap();
     println!("Approx: {}", approx);
+
+    println!("Elapsed time: {}", start_time.elapsed().unwrap().as_secs_f64());
 }
